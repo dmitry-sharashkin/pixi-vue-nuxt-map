db0e94240244450dfa76539697b64baa9f9d3513
Dmitry <faustfbs@mail.ru>
2022-02-25 19:09:29 +0300

    .

---
+++ components/PopupSigment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="popup-container">
+    <h1>Sigment PopUp</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.popup-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: red;
+}
+</style>
